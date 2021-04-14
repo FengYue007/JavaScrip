ccf3514e29047418bdb8ad6300cf3c9997141fb9
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-14 17:01:15 +0800

    timer-jquery-fanasty _ add slideDown slideUp prependTo

--- 05_timer-jquery/main.js
+++ 05_timer-jquery/main.js
@@ -5,11 +5,19 @@ $(document).ready(function() {
       if (timer) {
         clearInterval(timer)
         time = null;
+        $('button').text('Start')
+        $('#running').slideUp()
       } else {
         timer = setInterval(function() {
           cnt += 1;
-          $('#seconds').text(cnt)
-        }, 1000)
+          $('#seconds').text(Math.floor(cnt / 10))
+          
+          // $('.dots').prepend($('.dots div:last-child'))
+          $('.dots div:last-child').prependTo($('.dots'))
+        }, 100)
+
+        $('button').text('Stop')
+        $('#running').slideDown()
       }
     // $('#seconds').text('hello')
   })
