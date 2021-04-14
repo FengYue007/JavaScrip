889b79f74d34727e95ca44f8f7ea0271bac9e5d4
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-14 14:25:18 +0800

    create-element

--- 04_create-element/main.js
+++ 04_create-element/main.js
@@ -1,1 +1,12 @@
-// let's start coding:
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('create-element').addEventListener('click', function() {
+    const timeDiv = document.createElement('div');
+
+    // debugger
+    timeDiv.classList.add('time')
+    timeDiv.textContent = (new Date()).toString()
+    document.querySelector('.right').appendChild(timeDiv)
+  })
+})
+
+    debugger
