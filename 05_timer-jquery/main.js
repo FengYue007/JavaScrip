9050ca3e4f75cf9f8f005382eebae784281799ca
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-14 16:11:32 +0800

    add timer-jquery

--- 05_timer-jquery/main.js
+++ 05_timer-jquery/main.js
@@ -1,1 +1,11 @@
-// let's start coding:
+$(document).ready(function() {
+  $('button').click(function() {
+    let cnt = 0
+    setInterval(function() {
+      cnt += 1
+      $('#seconds').text(cnt)
+    }, 1000)
+    // $('#seconds').text('hello')
+
+  })
+})
